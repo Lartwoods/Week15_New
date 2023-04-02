cdd232f6a74aece7543b38c827254dfe5608c9ed
Vlada <lartwoods@gmail.com>
2023-04-03 00:26:38 +0300

    all

--- task2/task2_n_script.js
+++ task2/task2_n_script.js
@@ -1,16 +1,15 @@
 function processNumber() {
-  const input = document.getElementById("number-input").value;
-  const digits = Array.from(String(input), Number);
-
+  const myInput = document.querySelector("#number-input").value;
+  const numbers = Array.from(myInput, Number);
   const outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = `<p>Цифры числа: ${digits.join(", ")}</p>`;
 
-  const numDigits = digits.length;
-  const sumDigits = digits.reduce((a, b) => a + b, 0);
+  outputDiv.innerHTML = `<p>Цифры числа: ${numbers.join(", ")}</p>`;
+  const numDigits = numbers.length;
+
+  const sum = numbers.reduce((a, b) => a + b, 0);
   outputDiv.innerHTML += `<p>Количество цифр: ${numDigits}</p>`;
-  outputDiv.innerHTML += `<p>Сумма цифр: ${sumDigits}</p>`;
-
-  const reversedDigits = digits.reverse();
+  outputDiv.innerHTML += `<p>Сумма цифр: ${sum}</p>`;
+  const reversedDigits = numbers.reverse();
   outputDiv.innerHTML += `<p>Число с обратным порядком цифр: ${reversedDigits.join(
     ""
   )}</p>`;
